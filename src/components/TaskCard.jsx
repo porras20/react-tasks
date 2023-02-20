2e4ab71f57b02612c7879ff16a3e480f539d24ef
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-02-20 10:58:11 -0500

    Agregamos Tailwind al proyecto

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,7 +6,7 @@ export  function TaskCard({task}) {
   const {deleteTask} = useContext(TaskContext)
   return (
     <div>
-      <h1>{task.title}</h1>
+      <h1 className='text-3xl font-bold underline'>{task.title}</h1>
       <p>{task.description}</p>
       <button onClick={()=>deleteTask(task.id)}>Eliminar tarea</button>
     </div>
